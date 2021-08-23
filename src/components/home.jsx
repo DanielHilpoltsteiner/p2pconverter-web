e30bbe5322959434e6d6697d669777a8cbabf732
Aydin Tekin <aydin.tekin@ibsolution.de>
2021-08-23 17:29:06 +0200

    added tracker

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import FileDrop from "./filedrop";
-
 class Home extends Component {
   state = {};
 
--- src/components/page.jsx
+++ src/components/page.jsx
@@ -4,13 +4,24 @@ import Header from "./header";
 import Home from "./home";
 import About from "./about";
 import ReportBug from "./reportbug";
+import ReactGA from "react-ga";
+import createHistory from "history/createBrowserHistory";
 
+const history = createHistory();
 class Page extends Component {
   state = {};
 
+  componentDidMount() {
+    history.listen((location) => {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    });
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
-      <HashRouter>
+      <HashRouter history={history}>
         <Header href="/" />
         <Switch>
           <Route path={["/home", "/"]} exact render={(props) => <Home {...props} />} />
